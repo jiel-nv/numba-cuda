78b10fbc3a0554b9a6007024d62f3ec6423707aa
jiel-nv <jiel@nvidia.com>
2025-03-11 12:26:29 -0700

    Add a customized CUDA lowering pass.
    Change the codegen to stop emitting llvm.dbg.declare for local scalar variables
    when allocate space for them. Instead, emit llvm.dbg.value immediately after
    a store instruction is created for them.
    
    Use the same source level symbol name for all the variations / aliases of them.
    
    Merge the DILocalVariable nodes if they have the same scope, name and type, i.e.
    always use the line number from the first definition or assignment statement.

--- numba_cuda/numba/cuda/debuginfo.py
+++ numba_cuda/numba/cuda/debuginfo.py
@@ -1,5 +1,5 @@
 from llvmlite import ir
-from numba.core import types
+from numba.core import (types, cgutils)
 from numba.core.debuginfo import DIBuilder
 from numba.cuda.types import GridGroup
 
@@ -7,6 +7,11 @@ _BYTE_SIZE = 8
 
 
 class CUDADIBuilder(DIBuilder):
+
+    def __init__(self, module, filepath, cgctx, directives_only):
+        super().__init__(module, filepath, cgctx, directives_only)
+        # Cache for local variable metadata type and line deduplication
+        self._vartypelinemap = {}
 
     def _var_type(self, lltype, size, datamodel=None):
         is_bool = False
@@ -42,3 +47,52 @@ class CUDADIBuilder(DIBuilder):
 
         # For other cases, use upstream Numba implementation
         return super()._var_type(lltype, size, datamodel=datamodel)
+
+    def mark_variable(self, builder, allocavalue, name, lltype, size, line,
+                      datamodel=None, argidx=None):
+
+        if (name.startswith('$') or '.' in name):
+            # Do not emit llvm.dbg.declare on user variable alias
+            return
+        else:
+            int_type = ir.IntType,
+            real_type = ir.FloatType, ir.DoubleType
+            if (isinstance(lltype, int_type + real_type)):
+                # Attempt to not mixing llvm.dbg.value' with llvm.dbg.declare
+                # for the same scalar variable. So don't emit declare here.
+                return
+            else:
+                return super().mark_variable(builder, allocavalue, name, lltype,
+                                             size, line, datamodel, argidx)
+
+    def update_variable(self, builder, value, name, lltype, size, line,
+                        datamodel=None):
+        m = self.module
+        fnty = ir.FunctionType(ir.VoidType(), [ir.MetaDataType()] * 3)
+        decl = cgutils.get_or_insert_function(m, fnty, 'llvm.dbg.value')
+
+        mdtype = self._var_type(lltype, size, datamodel)
+        index = name.find('.')
+        if index >= 0:
+            name = name[:index]
+        # Attempt to merge DILocalVariable nodes with same name and type but
+        # different lines.
+        # Use the cached [(name, type) -> line] info to deduplicate metadata
+        # Use the lltype as part of key here, can instead use the mdtype if
+        # llvmlite PR#1165 get merged.
+        key = (name, lltype)
+        if key in self._vartypelinemap:
+            line = self._vartypelinemap[key]
+        else:
+            self._vartypelinemap[key] = line
+        mdlocalvar = m.add_debug_info('DILocalVariable', {
+            'name': name,
+            'arg': 0,
+            'scope': self.subprograms[-1],
+            'file': self.difile,
+            'line': line,
+            'type': mdtype,
+        })
+        mdexpr = m.add_debug_info('DIExpression', {})
+
+        return builder.call(decl, [value, mdlocalvar, mdexpr])
